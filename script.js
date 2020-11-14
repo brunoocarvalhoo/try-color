ed5f2daafbf400db93a5b1fa744f45e75de5c91f
brunoocarvalhoo <bruno.carvalho310303@gmail.com>
2020-11-14 16:43:31 -0300

    remove console.log

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ const divsBall = document.getElementsByClassName('ball');
 function numberRandom() {
     const number = Math.floor(Math.random() * 256);
     return number;
-};
+}//fim funcao
 
-function collorRandom(){
+function colorRandom(){
 
     const rgb = `rgb(${numberRandom()}, ${numberRandom()}, ${numberRandom()})`;
     return rgb;
+}//fim funcao
 
+for (let ball = 0; ball < divsBall.length; ball ++){
+divsBall[ball].style.backgroundColor = colorRandom();
 
 }
 
-console.log(numberRandom());
-console.log(collorRandom());
 
 
