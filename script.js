0e007bc626c5beba5ac2a23d6bebb226798448a7
brunoocarvalhoo <bruno.carvalho310303@gmail.com>
2020-11-14 16:38:52 -0300

    gera numero rgb aleatorio

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('oi');
+const divsBall = document.getElementsByClassName('ball');
+
+
+function numberRandom() {
+    const number = Math.floor(Math.random() * 256);
+    return number;
+};
+
+function collorRandom(){
+
+    const rgb = `rgb(${numberRandom()}, ${numberRandom()}, ${numberRandom()})`;
+    return rgb;
+
+
+}
+
+console.log(numberRandom());
+console.log(collorRandom());
+
+
